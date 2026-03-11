f6dba5a8e4cd358dbe8c59362b706c33ff2b9c50
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2026-03-11 15:14:18 +0200

    hot fix

--- components/layouts/clientSideLayout.tsx
+++ components/layouts/clientSideLayout.tsx
@@ -5,27 +5,34 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useTheme } from "next-themes"
 import React from "react"
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
-import { Toaster } from "sonner"
+import { Toaster as SonnerToaster } from "sonner"
 import { ThemeProvider } from "../theme-provider"
 
 const ClientSideLayout = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient()
-  const { theme } = useTheme()
   useFirebaseNotifications()
-
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster
-        position="bottom-right"
-        richColors
-        theme={theme as "system" | "light" | "dark"}
-      />
       <GoogleReCaptchaProvider
         reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <Toaster />
+          {children}
+        </ThemeProvider>
       </GoogleReCaptchaProvider>
     </QueryClientProvider>
+  )
+}
+
+const Toaster = () => {
+  const { theme } = useTheme()
+  return (
+    <SonnerToaster
+      position="bottom-right"
+      richColors
+      theme={theme as "system" | "light" | "dark"}
+    />
   )
 }
 
