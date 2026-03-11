e518f1f53c4c0fd0744fb2db9030744fdec9b78a
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2026-03-11 04:53:31 +0200

    hot fix

--- app/auth/_comps/ReCAPTCHA.tsx
+++ app/auth/_comps/ReCAPTCHA.tsx
@@ -3,8 +3,8 @@ import GoogleCaptcha from "react-google-recaptcha"
 const ReCAPTCHA = ({ onChange }: { onChange: (value: string) => void }) => {
   return (
     <GoogleCaptcha
-      sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-      onChange={onChange}
+      sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+      onChange={(token) => onChange(token || "")}
     />
   )
 }
