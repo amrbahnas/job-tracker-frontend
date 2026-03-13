3358377cfb0896b9e7417953e462d5de38eb533a
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2026-03-13 22:57:32 +0200

    hot fix

--- app/auth/_comps/index.tsx
+++ app/auth/_comps/index.tsx
@@ -3,25 +3,30 @@ import Container from "@/components/ui/container"
 import { cn } from "@/lib/utils"
 import { AuthFormSection } from "./authFormSection"
 import { AuthPromo } from "./authPromo"
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
 
 export default function Auth() {
   return (
-    <Container className={cn("flex flex-1 items-center justify-center")}>
-      <main
-        className={cn(
-          "flex h-fit flex-col gap-8 bg-card",
-          "w-full gap-0 overflow-hidden rounded-2xl border border-border bg-card shadow-lg sm:max-w-5xl md:flex-row"
-        )}
-        role="main"
-        aria-label="Authentication"
-      >
-        <aside className="hidden flex-2 sm:block md:shrink-0">
-          <AuthPromo className="h-full w-full" />
-        </aside>
-        <div className="order-1 flex flex-col justify-between sm:flex-3">
-          <AuthFormSection />
-        </div>
-      </main>
-    </Container>
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+    >
+      <Container className={cn("flex flex-1 items-center justify-center")}>
+        <main
+          className={cn(
+            "flex h-fit flex-col gap-8 bg-card",
+            "w-full gap-0 overflow-hidden rounded-2xl border border-border bg-card shadow-lg sm:max-w-5xl md:flex-row"
+          )}
+          role="main"
+          aria-label="Authentication"
+        >
+          <aside className="hidden flex-2 sm:block md:shrink-0">
+            <AuthPromo className="h-full w-full" />
+          </aside>
+          <div className="order-1 flex flex-col justify-between sm:flex-3">
+            <AuthFormSection />
+          </div>
+        </main>
+      </Container>
+    </GoogleReCaptchaProvider>
   )
 }
--- components/layouts/clientSideLayout.tsx
+++ components/layouts/clientSideLayout.tsx
@@ -1,35 +1,23 @@
 "use client"
 
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import useFirebaseNotifications from "@/hooks/useFirebaseNotifications"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useTheme } from "next-themes"
 import React from "react"
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
+
 import { Toaster as SonnerToaster } from "sonner"
 import { ThemeProvider } from "../theme-provider"
 
 const ClientSideLayout = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient()
-  const isRecaptchaEnabled = !!process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
   return (
     <QueryClientProvider client={queryClient}>
       {/* <ReactQueryDevtools /> */}
-      {isRecaptchaEnabled ? (
-        <GoogleReCaptchaProvider
-          reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
-        >
-          <ThemeProvider>
-            <Toaster />
-            {children}
-          </ThemeProvider>
-        </GoogleReCaptchaProvider>
-      ) : (
-        <ThemeProvider>
-          <Toaster />
-          {children}
-        </ThemeProvider>
-      )}
+
+      <ThemeProvider>
+        <Toaster />
+        {children}
+      </ThemeProvider>
     </QueryClientProvider>
   )
 }
