1cd6d3b54742dc690e236f6c63773dd917550556
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2026-03-17 12:36:52 +0200

    hot fix

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,5 @@
-export const locales = ["en", "ar"] as const;
+export const locales = ["en", "ar"] as const
 
-export type Locale = (typeof locales)[number];
+export type Locale = (typeof locales)[number]
 
-export const defaultLocale: Locale = "en";
+export const defaultLocale: Locale = "ar"
