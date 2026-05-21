bc426bfae239a36e9f3e1bd804d27ec81174e03d
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2026-05-21 11:39:18 +0300

    try fix

--- api_config/proxyClient.ts
+++ api_config/proxyClient.ts
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const proxyAxiosInstance = axios.create({
-  baseURL: "/api/proxy",
+  // baseURL: "/api/proxy",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+
   withCredentials: true,
 })
 
